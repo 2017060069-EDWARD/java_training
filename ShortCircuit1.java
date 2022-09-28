3429f09e60190c169dee76519f8a122242239e20
Ms Mya <eddykabwemba@gmail.com>
2022-09-28 14:47:20 +0200

    2809

---
+++ ShortCircuit1.java
@@ -0,0 +1,8 @@
+public class ShortCircuit2 {
+    public static void main(String[] args) {
+ 	   boolean a = flase;
+        boolean b = true;
+       
+        System.out.println(a && b);
+    }
+}
